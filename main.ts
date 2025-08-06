9d76da9199882b5a8293b9581e26930b675ccd92
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-08-06 12:51:43 +0200

    resend-hono

---
+++ main.ts
@@ -0,0 +1,30 @@
+// Import Hono
+import { Hono } from 'jsr:@hono/hono'
+
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY');
+
+const app = new Hono();
+
+app.post('/', async (c) => {
+  const { to, subject, html } = await c.req.json();
+
+  const response = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${RESEND_API_KEY}`
+    },
+    body: JSON.stringify({
+      from: 'resend <onboarding@resend.dev>',
+      to,
+      subject,
+      html
+    })
+  });
+
+  const data = await response.json();
+
+  return c.json(data);
+});
+
+Deno.serve(app.fetch);
